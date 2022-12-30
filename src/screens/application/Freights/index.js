4eeab975de159c76e0300a9359f5ac941499f034
EduardoColissi <educolissi@hotmail.com>
2022-12-30 19:42:11 -0300

    TRUCK-27 - Inicio da tela de fretes

--- src/screens/application/Freights/index.js
+++ src/screens/application/Freights/index.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
 
-const Freights = () => {
-  return (
+import api from "../../../services/api";
+
+import Loading from "../../../components/Loading";
+import FreightCard from "../../../components/FreightCard";
+
+function Freights() {
+  const [freights, setFreights] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const getFreights = async () => {
+    // setLoading(true);
+    try {
+      const response = await api.get("/freights/all");
+      setFreights(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    // setLoading(false);
+  };
+
+  console.log(freights, "aaaa");
+
+  return loading ? (
+    <Loading visible={loading} />
+  ) : (
     <View style={styles.screen}>
-      <Text>Freights</Text>
+      <FreightCard>
+        <Text style={styles.freightTitle}>Frete - 01</Text>
+      </FreightCard>
     </View>
   );
-};
+}
 
 export default Freights;
