2692e108a1ddd3e57d6c67943225a038d1de7c30
EduardoColissi <educolissi@hotmail.com>
2023-01-02 12:34:25 -0300

    TRUCK-27 - Finalizado tela de fretes

--- src/screens/application/Freights/index.js
+++ src/screens/application/Freights/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Button } from "react-native";
 import styles from "./styles";
-
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import api from "../../../services/api";
 
 import Loading from "../../../components/Loading";
 import FreightCard from "../../../components/FreightCard";
+import FreightFilterButton from "../../../components/FreightFilterButton";
 
 const Freights = () => {
   const [freights, setFreights] = useState();
@@ -22,6 +23,7 @@ const Freights = () => {
       } finally {
         setLoading(false);
       }
+      console.log(freights);
     };
     getFreights();
   }, []);
@@ -29,14 +31,36 @@ const Freights = () => {
   return loading ? (
     <Loading visible={loading} />
   ) : (
-    <View style={styles.screen}>
+    <View>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <FreightFilterButton
+          title="Nacional"
+          onPress={() => {
+            console.log("Nacional");
+          }}
+        />
+        <FreightFilterButton
+          title="Internacional"
+          onPress={() => {
+            console.log("Internacional");
+          }}
+        />
+      </View>
       <ScrollView>
         {freights.map((freight) => (
           <TouchableOpacity key={freight.id}>
             <FreightCard>
               <Text style={styles.freightTitle}>{freight.code}</Text>
-              <Text>{freight.description}</Text>
-              <Text>R$ {freight.value}</Text>
+              <Text>Valor: R$ {freight.value}</Text>
+              <View style={{ flexDirection: "row" }}>
+                <Text>Pontos: </Text>
+                <Text>{freight.points} </Text>
+                <FontAwesome5 name="coins" size={15} />
+              </View>
+              <Text>Origem: {freight.origin}</Text>
+              <Text>Destino: {freight.destiny}</Text>
+              <Text>Data de coleta: {freight.date}</Text>
+              <Text>Data de entrega: {freight.deadline}</Text>
             </FreightCard>
           </TouchableOpacity>
         ))}
--- src/screens/application/Freights/styles.js
+++ src/screens/application/Freights/styles.js
@@ -1,9 +1,6 @@
 import { StyleSheet } from "react-native";
 
 export default styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
   freightTitle: {
     fontSize: 20,
     fontWeight: "bold",
